dd80f11524aa80a955a07491d62a9acf92a16de9
Shanrif <89885486+Shanrif@users.noreply.github.com>
2023-10-06 19:58:49 -0700

    Fixed this all

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 function sum(a) {
-    var sum = a[0];
+    //changed how sum was initialized because it needed to be zero and it was not.
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
